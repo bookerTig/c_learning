9b7f0296409ed8b155bdac787503992a898eb366
bookerTig <booker_code@outlook.com>
2022-10-04 16:24:03 +0800

    new file:   10/10.13.6.c

---
+++ 10/10.13.6.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void reverse_order(double *n);
+int main()
+{
+
+    double n[10] = {5.5, 6.3, 8.2, 6.4, 12.6, 1.1, 9.6, 18.2, 5.6, 7.3};
+    int i;
+
+    //print the example array
+    printf("The array is {");
+    for (i = 0; i < 10; i++)
+        printf("%3.1f,", n[i]);
+    printf("\b}\n");
+
+
+    return 0;
+}
+
+//The answer function is here
+void reverse_order(double *n){
+    double answer[10];
+    ;
+    ;
+}
